a30ebbba846cd0afb27289d83986ddc1a2f6cad4
Jeff Craig <jeffcraig35@gmail.com>
2018-10-04 12:20:34 -0400

    Add games component placeholder

---
+++ client/src/Games.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Games.css';
+
+class Games extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>Test</div>
+        )
+    }
+}
+
+export default Games;
