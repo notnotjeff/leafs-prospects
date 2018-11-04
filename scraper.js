c192d636c0648118c36de252feff8354574064ba
Jeff Craig <jeffcraig35@gmail.com>
2018-11-04 17:22:35 -0500

    Add DST checker to both scrapers, will also check if at 2am or not

--- scraper.js
+++ scraper.js
@@ -21,6 +21,7 @@ admin.initializeApp({
 // Get And Set Backend Prospect Array
 const prospects = backend.prospects;
 
+// HELPER FUNCTIONS
 function getAge(dateString)
 {
     var today = new Date();
@@ -35,16 +36,37 @@ function getCurrentSeason() {
   return `${date.getFullYear()}-${(date.getFullYear() + 1).toString().substr(-2)}`;
 }
 
-Date.prototype.stdTimezoneOffset = function () {
-  var jan = new Date(this.getFullYear(), 0, 1);
-  var jul = new Date(this.getFullYear(), 6, 1);
-  return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+// FUNCTIONS FOR DAYLIGHT SAVINGS TIME
+function isDaylightSavings(today) {
+  const dstStart = getDateOfSundayInMonth(2, 3);
+  const dstEnd = getDateOfSundayInMonth(1, 11);
+  const offsetHours = today.getTimezoneOffset() === 0 ? 0 : 4; // If run locally in EST you need to offset for time difference from UTC
+
+  // If today is the start of daylight savings, check if it's past 2AM EST from UTC (should be 5 hours ahead during non-DST period)
+  if (`${today.getMonth()}-${today.getDate()}` === `${dstStart.getMonth()}-${dstStart.getDate()}`) {
+    return today.getHours() >= (7 - offsetHours) ? true : false;
+  // If today is end of daylight savings, check if it's past 2AM EST from UTC (should be 4 hours ahead during DST period)
+  } else if (`${today.getMonth()}-${today.getDate()}` === `${dstEnd.getMonth()}-${dstEnd.getDate()}`) {
+    return today.getHours() >= (6 - offsetHours) ? false : true;
+  // Else check if it is in between the DST period and return the proper value
+  } else {
+    return today < dstEnd && today >= dstStart ? true : false;
+  }
 }
 
-Date.prototype.isDstObserved = function () {
-  return this.getTimezoneOffset() < this.stdTimezoneOffset();
+function getDateOfSundayInMonth(sundayNumber, month) {
+  // Format variables from real world values to computer values
+  sundayNumber = (sundayNumber - 1) * 7;
+  month = month - 1;
+
+  const currentYear = new Date().getFullYear();
+  const start = new Date(currentYear, month, sundayNumber);
+  const sunday = sundayNumber + (7 - start.getDay());
+
+  return new Date(currentYear, month, sunday); 
 }
 
+// SCRAPING FUNCTION
 function scrape(prospects) {
   let promises = [];
 
@@ -237,7 +259,7 @@ async function updateDB() {
     let hours = "";
     let minutes = day.getMinutes() < 10 ? `0${day.getMinutes()}` : `${day.getMinutes()}`;
 
-    let offsetHours = day.isDstObserved() ? 5 : 4
+    let offsetHours = isDaylightSavings(day) ? 4 : 5;
 
     if (+day.getTimezoneOffset() === 0) { day.setHours(day.getHours() - offsetHours) }
     
