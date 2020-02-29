a4c6794713816736db3373706333e888251da9aa
jefftcraig <jeffcraig35@gmail.com>
2020-02-29 18:34:05 -0500

    Updates

---
+++ precache-manifest.845ecd58dd15306aef1dfc6f77bf8018.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6da141da35fe512f0a2e6780fd80eac0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34afae5d824b1e6f3619",
+    "url": "/static/css/main.b877f777.chunk.css"
+  },
+  {
+    "revision": "4b6af55265d547c27697",
+    "url": "/static/js/2.bb8f484f.chunk.js"
+  },
+  {
+    "revision": "7f3d623d03408f9350c3b5e194511d01",
+    "url": "/static/js/2.bb8f484f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34afae5d824b1e6f3619",
+    "url": "/static/js/main.2990538b.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
