5ad30583e4a47cf836090055947f9313cb2515cb
jefftcraig <jeffcraig35@gmail.com>
2021-01-03 20:59:18 -0500

    Change NLA to NL and add 2015 draft year back

--- src/constants/filter-options.js
+++ src/constants/filter-options.js
@@ -13,7 +13,7 @@ const leagues = [
   'Mestis',
   'MHL',
   'NCAA',
-  'NLA',
+  'NL',
   'OHL',
   'QMJHL',
   'Sarja20',
@@ -30,6 +30,6 @@ const shoots = ['Any', 'L', 'R']
 
 const rounds = ['Any', 1, 2, 3, 4, 5, 6, 7, 'Undrafted']
 
-const draft_years = ['Any', 2020, 2019, 2018, 2017, 2016, 'Undrafted']
+const draft_years = ['Any', 2020, 2019, 2018, 2017, 2016, 2015, 'Undrafted']
 
 export { leagues, positions, shoots, rounds, draft_years }
