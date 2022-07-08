20519571ee054b82032810212e96d2e3afadad0b
Jeff Craig <jeffcraig35@gmail.com>
2022-07-08 16:33:54 -0400

    Add 2022 seasons

--- src/constants/filter-options.js
+++ src/constants/filter-options.js
@@ -30,6 +30,6 @@ const shoots = ['Any', 'L', 'R']
 
 const rounds = ['Any', 1, 2, 3, 4, 5, 6, 7, 'Undrafted']
 
-const draft_years = ['Any', 2021, 2020, 2019, 2018, 2017, 2016, 2015, 'Undrafted']
+const draft_years = ['Any', 2022, 2021, 2020, 2019, 2018, 2017, 2016, 'Undrafted']
 
 export { leagues, positions, shoots, rounds, draft_years }
